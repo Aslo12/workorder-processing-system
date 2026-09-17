ef1d7353df298c8e9606d7bd020587342faa07ab
Ashutosh <107853164+Aslo12@users.noreply.github.com>
2026-09-17 13:16:25 +0530

    Create Inventory table in ProductionDB

---
+++ database/06_Inventory.sql
@@ -0,0 +1,10 @@
+USE ProductionDB;
+
+CREATE TABLE Inventory (
+    atr_key CHAR(36) PRIMARY KEY,
+    MaterialId VARCHAR(100),
+    MaterialCode VARCHAR(100),
+    WorkOrder VARCHAR(50),
+    MaterialQty INT,
+    BookingQty INT
+);
